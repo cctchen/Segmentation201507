4e8e94ca9494071eebe4d516a2ec0a31b27a3940
Tom <chentao.sc@gmail.com>
2015-07-31 00:06:47 +0800

    ADD POINT

--- Main_interface.m
+++ Main_interface.m
@@ -22,7 +22,7 @@ function varargout = Main_interface(varargin)
 
 % Edit the above text to modify the response to help Main_interface
 
-% Last Modified by GUIDE v2.5 28-Jul-2015 23:30:55
+% Last Modified by GUIDE v2.5 30-Jul-2015 23:51:57
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -95,7 +95,8 @@ guidata(hObject,handles);
 function listbox1_Callback(hObject, eventdata, handles)
 index_selected = get(handles.listbox1,'Value');
 axes(handles.axes1);
-imshow(handles.Dicom_list{index_selected});
+dcm_pic=dicomread(handles.Dicom_list{index_selected});
+imshow(dcm_pic);
 p_Dilate=get(handles.slider1,'value');
 handles.p_Dilate=p_Dilate;
 handles.index_selected=index_selected;
@@ -178,6 +179,7 @@ imshow(BWoutline); hold on
 function pushbutton2_Callback(hObject, eventdata, handles)
 axes(handles.axes2);
 % read two curves
+set(handles.text1,'string','Select low neck arc.');
 Low_rect = getrect;
 Low_rect = round(Low_rect); %transfer into integer
 Lminx=Low_rect(1);
@@ -188,6 +190,8 @@ Lmaxy=Low_rect(2) + Low_rect(4);
 Lx=Lx+Lminx-1; %transfer into the local coordinate
 Ly=Ly+Lminy-1;
 
+
+set(handles.text1,'string','Select high neck arc.');
 High_rect = getrect;
 High_rect = round(High_rect);
 Hminx=High_rect(1);
@@ -238,8 +242,11 @@ End_y2 = M_y + round(sin(seta)*Length_F);
 
 plot([End_x1, End_x2], [End_y1, End_y2], 'r-');
 
+
 %Find axis of the femur shaft
+set(handles.text1,'string','Select Shaft area');
 End_rect = getrect;
+set(handles.text1,'string','');
 End_rect = round(End_rect);
 End_minx=End_rect(1);
 End_maxx=End_rect(1) + End_rect(3);
@@ -279,25 +286,123 @@ plot(Cp(1),Cp(2),'*');
 
 V1=(Mid_x(E_n) - Cp(1) + ((Mid_y(E_n))-Cp(2))*i)/abs(Mid_x(E_n) - Cp(1) + ((Mid_y(E_n))-Cp(2))*i);
 V2=(End_x1 - Cp(1) + (End_y1-Cp(2))*i)/abs(End_x1 - Cp(1) + (End_y1-Cp(2))*i);
-Vplus1=50*(V1+V2);
-Intetrochanter_x1=Cp(1)+real(Vplus1);
-Intetrochanter_y1=Cp(2)+imag(Vplus1);
+
+[tmp,size_outl]=size(handles.BWoutline);
+Intetrochanter_x1=Cp(1);
+Intetrochanter_y1=Cp(2);
+r=1;
+while((Intetrochanter_x1 < size_outl) && (Intetrochanter_x1 > 0) && (handles.BWoutline(Intetrochanter_y1,Intetrochanter_x1) ~= 1))
+    Vplus1=r*(V1+V2);
+    Intetrochanter_x1=round(Cp(1)+real(Vplus1));
+    Intetrochanter_y1=round(Cp(2)+imag(Vplus1));
+    r=r+0.5;
+end
 %line([Cp(1),Intetrochanter_x1],[Cp(2),Intetrochanter_y1]);
+r=-1;
+Intetrochanter_x2=Cp(1);
+Intetrochanter_y2=Cp(2);
+while((Intetrochanter_x2 < size_outl) && (Intetrochanter_x2 > 0) && handles.BWoutline(Intetrochanter_y2,Intetrochanter_x2) ~= 1)
+    Vplus2=r*(V1+V2);
+    Intetrochanter_x2=round(Cp(1)+real(Vplus2));
+    Intetrochanter_y2=round(Cp(2)+imag(Vplus2));
+    r=r-0.5;
+end
 
-Vplus2=-Vplus1;
-Intetrochanter_x2=Cp(1)+real(Vplus2);
-Intetrochanter_y2=Cp(2)+imag(Vplus2);
-line([Intetrochanter_x1,Intetrochanter_x2],[Intetrochanter_y1,Intetrochanter_y2]);
+plot([Intetrochanter_x1,Intetrochanter_x2],[Intetrochanter_y1,Intetrochanter_y2], '*-');
 
-line([Cp(1)-20,Cp(1)+20],[Cp(2)+1.5*sqrt(d_min),Cp(2)+1.5*sqrt(d_min)]);
+[shafty,shaftx]=find(handles.BWoutline(Cp(2)+round(1.5*sqrt(d_min)),:));
+
+shafty=shafty+Cp(2)+round(1.5*sqrt(d_min));
+
+%line([Cp(1)-20,Cp(1)+20],[Cp(2)+1.5*sqrt(d_min),Cp(2)+1.5*sqrt(d_min)]);
+plot(shaftx,shafty,'*');
+
 axes(handles.axes2);
 imshow(handles.I+im2uint16(handles.BWoutline));hold on
 plot([Px_L, Px_H], [Py_L, Py_H], 'r*-'); hold on
 plot([End_x1, End_x2], [End_y1, End_y2], 'r-');
 plot([Cp(1),Mid_x(E_n)],[Cp(2)-20,Mid_y(E_n)],  'g-');
 plot(Cp(1),Cp(2),'*');
-line([Intetrochanter_x1,Intetrochanter_x2],[Intetrochanter_y1,Intetrochanter_y2]);
-line([Cp(1)-20,Cp(1)+20],[Cp(2)+1.5*sqrt(d_min),Cp(2)+1.5*sqrt(d_min)]);
+plot([Intetrochanter_x1,Intetrochanter_x2],[Intetrochanter_y1,Intetrochanter_y2], '*-');
+%line([Cp(1)-20,Cp(1)+20],[Cp(2)+1.5*sqrt(d_min),Cp(2)+1.5*sqrt(d_min)]);
+plot(shaftx,shafty,'r*-');
+axes(handles.axes3);
+improfile(handles.I, [Px_L, Px_H], [Py_L, Py_H]);
+axes(handles.axes4);
+improfile(handles.I, [Intetrochanter_x1,Intetrochanter_x2],[Intetrochanter_y1,Intetrochanter_y2]);
+axes(handles.axes5);
+improfile(handles.I, shaftx,shafty);
+set(handles.edit1,'string',strcat('NFN_L_x=',int2str(Px_L),', NFN_L_y=',int2str(Py_L),'; NFN_H_x=',int2str(Px_H),', NFN_H_y=',int2str(Py_H)));
+set(handles.edit2,'string',strcat('IT_L_x=',int2str(Intetrochanter_x1),', IT_L_y=',int2str(Intetrochanter_y1),'; IT_H_x=',int2str(Intetrochanter_x2),', IT_H_y=',int2str(Intetrochanter_y2)));
+set(handles.edit3,'string',strcat('FS_L_x=',int2str(shaftx(1)),', FS_L_y=',int2str(shafty(1)),'; FS_R_x=',int2str(shaftx(2)),', FS_R_y=',int2str(shafty(2))));
+
 % hObject    handle to pushbutton2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
